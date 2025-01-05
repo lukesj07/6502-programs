6ae15115b14a948fed2cfb3b10cb6a4c3046f774
lukesj07 <lukesjennings07@gmail.com>
2025-01-04 22:12:12 -0500

    random mine generation, title screen, and some comment changes. need optimization still

--- minesweeper.s
+++ minesweeper.s
@@ -1,129 +1,249 @@
-; VIA Port Definitions
+; VIA ports
 PORTB = $6000
 PORTA = $6001
 DDRB  = $6002
 DDRA  = $6003
-PCR   = $600c  ; Peripheral control register
-IFR   = $600d  ; Interrupt flag register
-IER   = $600e  ; Interrupt enable register
+PCR   = $600c  ; peripheral control register
+IFR   = $600d  ; interrupt flag register
+IER   = $600e  ; interrupt enable register
 
-; LCD Control Bits
-E  = %10000000  ; Enable bit
-RW = %01000000  ; Read/Write bit
-RS = %00100000  ; Register Select bit
+; LCD control bits
+E  = %10000000  ; enable bit
+RW = %01000000  ; read/write bit
+RS = %00100000  ; register select bit
 
-; Button Input Masks (Port A)
+; button input masks (port a)
 LEFT  = %00001110
 RIGHT = %00001101
 UP    = %00001011
 DOWN  = %00000111
 
-; Interrupt Flags
-MOVE_INT   = %00000010  ; Movement interrupt flag
-SELECT_INT = %00010000  ; Selection interrupt flag
+; interrupt flags
+MOVE_INT   = %00000010  ; movement interrupt flag
+SELECT_INT = %00010000  ; selection interrupt flag
 
-; Memory Layout (Page 2 and 3)
-mine_array      = $0200  ; 8 bytes - Mine positions ($0200-$0207)
-revealed_array  = $0208  ; 8 bytes - Revealed cells ($0208-$020F)
-cursor_x        = $0210  ; 1 byte - Current cursor X position
-cursor_y        = $0211  ; 1 byte - Current cursor Y position
-bitmask         = $0212  ; 1 byte - Working bitmask
-loc_mine_count  = $0213  ; 1 byte - Local mine counter
-mine_count      = $0214  ; 1 byte - Total number of mines
-cells_revealed  = $0215  ; 1 byte - Number of revealed cells
-buff_ptr        = $0216  ; 1 byte - Display buffer pointer
-temp            = $0217  ; 1 byte - Temporary storage
-neighbor_x      = $0218  ; 1 byte - Neighbor X coordinate
-neighbor_y      = $0219  ; 1 byte - Neighbor Y coordinate
-neighbor_ptr    = $021A  ; 1 byte - Neighbor loop counter
-blink_counter   = $021B  ; 1 byte - Counter for cursor blink
+; memory layout (page 2 and 3)
+mine_array      = $0200  ; 8 bytes - mine positions ($0200-$0207)
+revealed_array  = $0208  ; 8 bytes - revealed cells ($0208-$020F)
+cursor_x        = $0210  ; 1 byte - current cursor x
+cursor_y        = $0211  ; 1 byte - current cursor y
+bitmask         = $0212  ; 1 byte - working bitmask
+loc_mine_count  = $0213  ; 1 byte - local mine counter
+mine_count      = $0214  ; 1 byte - total number of mines
+cells_revealed  = $0215  ; 1 byte - number of revealed cells
+buff_ptr        = $0216  ; 1 byte - display buffer pointer
+temp            = $0217  ; 1 byte - temporary storage
+neighbor_x      = $0218  ; 1 byte - neighbor X coordinate
+neighbor_y      = $0219  ; 1 byte - neighbor Y coordinate
+neighbor_ptr    = $021A  ; 1 byte - neighbor loop counter
+blink_counter   = $021B  ; 1 byte - counter for cursor blink
+startup_timer   = $021C  ; 1 byte - timer used as a random seed for mine generation
+game_started    = $021D  ; 1 byte - flag for game state
 display_buffer  = $0300  ; 64 bytes for display ($0300-$033F)
 number_array    = $0340  ; 64 bytes for mine numbers ($0340-$037F)
 
-; Program Start
   .org $8000
 
-; Game Data
 win_message: .asciiz "You Won!"
 lose_message: .asciiz "You Lost."
+title_msg:     .asciiz "MINESWEEPER"
+press_msg:     .asciiz "Press SELECT"
 
 ; ====================================
 ; Initialization
 ; ====================================
 reset:
-  sei                     ; Disable interrupts during init
+  sei                     ; disable interrupts during init
   ldx #$ff
-  txs                     ; Initialize stack pointer
+  txs                     ; initialize stack pointer
 
-  ; Initialize VIA
-  lda #%00000000         ; Set interrupts to falling edge
+  ; initialize VIA
+  lda #%00000000         ; set interrupts to falling edge
   sta PCR
-  lda #%10010010         ; Enable CA1 and CB1 interrupts
+  lda #%10010010         ; enable CA1 and CB1 interrupts
   sta IER
 
-  ; Set up I/O ports
+  ; set up io ports
   lda #%11111111         ; PORTB: all output
   sta DDRB
   lda #%11100000         ; PORTA: top 3 pins output
   sta DDRA
 
-  ; Initialize LCD
+  ; init LCD
   lda #%00111000         ; 8-bit mode, 2-line display, 5x8 font
   jsr execute_lcd_instruction
-  lda #%00001100         ; Display on, cursor off
+  lda #%00001100         ; display on, cursor off
   jsr execute_lcd_instruction
-  lda #%00000110         ; Increment cursor, don't shift display
+  lda #%00000110         ; increment cursor, don't shift display
   jsr execute_lcd_instruction
-  lda #$00000001         ; Clear display
+  lda #$00000001         ; clear display
   jsr execute_lcd_instruction
 
-  ; Initialize game state
+  ; init game state
   lda #0
-  sta cursor_x
-  sta cursor_y
-  sta cells_revealed
-  sta blink_counter
+  sta game_started       ; clear game started flag
+  sta startup_timer      ; clear startup timer
 
-  ; Clear arrays
+  jsr show_title_screen  ; show title screen
+  cli                    ; enable interrupts to detect SELECT
+  jmp startup_loop       ; enter startup loop
+
+; show title screen
+show_title_screen:
+  ; center "MINESWEEPER" on first line
+  lda #%00000010         ; cursor home
+  jsr execute_lcd_instruction
+  lda #" "               ; 3 spaces for centering
+  jsr print_char
+  jsr print_char
+  jsr print_char
+  
   ldx #0
-mine_setup:
+title_loop:
+  lda title_msg, x
+  beq title_done
+  jsr print_char
+  inx
+  jmp title_loop
+title_done:
+
+  ; "Press SELECT" on second line
+  lda #%11000000         ; move to second line
+  jsr execute_lcd_instruction
+  lda #" "               ; 2 spaces for centering
+  jsr print_char
+  jsr print_char
+  
+  ldx #0
+press_loop:
+  lda press_msg, x
+  beq press_done
+  jsr print_char
+  inx
+  jmp press_loop
+press_done:
+  rts
+
+; startup loop with timer
+startup_loop:
+  inc startup_timer      ; increment timer
+  
+  ; check if SELECT was pressed (via interrupt)
+  lda game_started
+  beq startup_loop       ; If not started, continue loop
+
+  sei                   ; disable interrupts during debounce delay and during generation
+
+  ldx #$32              ; delay should be around 50 ms
+outer_delay:
+  ldy #$FF
+inner_delay:
+  dey
+  bne inner_delay
+  dex
+  bne outer_delay
+  
+  ; start game setup
+  jsr clear_board
+  jsr place_mines
+  jmp init_game
+
+
+clear_board:
+  ldx #0
+clear_loop:
   lda #0
   sta mine_array, x
   sta revealed_array, x
   sta number_array, x
   inx
   cpx #8
-  bne mine_setup
-
-  ; Initialize remaining number array
+  bne clear_loop
+  
+  ; clear remaining number array
   ldx #8
-continue_number_init:
+continue_number_clear:
   lda #0
   sta number_array, x
   inx
   cpx #64
-  bne continue_number_init
+  bne continue_number_clear
+  rts
 
-  ; Set up initial mine positions
-  lda #10
+; generate a random number using timer value
+get_random:
+  lda startup_timer
+  asl
+  bcc no_eor
+  eor #$1D
+no_eor:
+  sta startup_timer      ; store back the rotated value
+  rts
+
+; place mines randomly
+place_mines:
+  lda #0
   sta mine_count
+place_loop:
+  lda mine_count
+  cmp #10                ; check if 10 mines have been placed
+  beq mines_done
   
-  lda #%01010000
-  sta mine_array
-  sta mine_array + 6
-  lda #%00001000
-  sta mine_array + 2
-  sta mine_array + 3
-  lda #%00000011
-  sta mine_array + 4
-  lda #%10000001
-  sta mine_array + 7
+  jsr get_random         ; get random x position (0-7)
+  and #%00000111
+  tax                    ; use x register for x position
+  
+  jsr get_random         ; get random y position (0-7)
+  and #%00000111
+  tay                    ; use y register for y position
+  
+  ; create bitmask for this position
+  txa                    ; get x position back
+  eor #7
+  tax
+  lda #1
+  sta bitmask
+shift_mine:
+  cpx #0
+  beq check_position
+  asl bitmask
+  dex
+  jmp shift_mine
 
+check_position:
+  tya                    ; get y position back for array index
+  tax
+  lda mine_array, x      ; check if position already has mine
+  and bitmask
+  bne place_loop         ; if mine exists, try again
+  
+  ; place mine
+  lda mine_array, x
+  ora bitmask
+  sta mine_array, x
+  inc mine_count
+  jmp place_loop
+
+mines_done:
+  rts
+
+
+init_game:
+  lda #0
+  sta cursor_x
+  sta cursor_y
+  sta cells_revealed
+  sta blink_counter
+  
+  lda #%00000001         ; clear display before starting game
+  jsr execute_lcd_instruction
+  
+  cli                    ; enable interrupts
 ; ====================================
 ; Main Game Loop
 ; ====================================
 game_loop:
-  ; Check win condition
+  ; check if won
   lda #64
   sec
   sbc mine_count
@@ -142,7 +262,7 @@ game_loop:
 ; ====================================
 won:
   ldx #0
-  lda #%00000001         ; Clear display
+  lda #%00000001         ; clear display
   jsr execute_lcd_instruction
 won_loop:
   lda win_message, x
@@ -153,7 +273,7 @@ won_loop:
 
 lost:
   ldx #0
-  lda #%00000001         ; Clear display
+  lda #%00000001         ; clear display
   jsr execute_lcd_instruction
 lost_loop:
   lda lose_message, x
@@ -170,18 +290,18 @@ done:
 ; ====================================
 
 update_display:
-  lda cursor_y           ; Determine which section to display
-  lsr                    ; Divide by 2 to get section index (0-3)
+  lda cursor_y           ; determine which section to display
+  lsr                    ; divide by 2 to get section index (0-3)
   tax
-  lda section_offsets,x  ; Load the corresponding offset
-  tax                    ; Put offset in X for show routine
-  jmp show              ; Continue with show routine
+  lda section_offsets,x  ; load the corresponding offset
+  tax                    ; put offset in x for show routine
+  jmp show               ; continue with show routine
 show:
-  lda #%00000010         ; Return cursor home
+  lda #%00000010         ; cursor home
   jsr execute_lcd_instruction
 
   ldy #0
-show_loop:               ; Display first line
+show_loop:               ; display first line
   lda display_buffer, x
   jsr print_char
   inx
@@ -192,7 +312,7 @@ show_loop:               ; Display first line
 
 section_offsets: .byte 0, 16, 32, 48
 
-print_page:              ; Add section number
+print_page:              ; add section number
   lda #" "
   jsr print_char
   jsr print_char
@@ -207,14 +327,14 @@ print_page:              ; Add section number
   lda #"2"
   cpx #24
   beq newline
-  lda #"1"              ; Default to 1
+  lda #"1"              ; default to 1
 
 newline:
   jsr print_char
-  lda #%11000000        ; Move to second line
+  lda #%11000000        ; move to second line
   jsr execute_lcd_instruction
 
-show_loop2:             ; Display second line
+show_loop2:             ; display second line
   lda display_buffer, x
   jsr print_char
   inx
@@ -227,15 +347,15 @@ show_loop2:             ; Display second line
 ; Display Buffer Update
 ; ====================================
 update_display_buffer:
-  sei ;                   ; Protect shared data
+  sei ;                   ; protect shared data
   lda #0
   sta buff_ptr
 
-  ldy #0                ; Row counter
+  ldy #0                ; row counter
 row_loop:
-  ldx #0                ; Column counter
+  ldx #0                ; column counter
 col_loop:
-  ; Calculate buffer position (y * 8) + x
+  ; calculate buffer position (y * 8) + x
   tya
   asl
   asl
@@ -245,24 +365,24 @@ col_loop:
   adc temp
   sta buff_ptr
   
-  ; Check cursor position
+  ; check cursor position
   cpx cursor_x
   bne not_cursor
   cpy cursor_y
   bne not_cursor
 
-  ; Blink logic
+  ; blink logic
   lda blink_counter
   and #%01000000 ; toggles every 64 game cycles
   bne not_cursor
   
   ldx buff_ptr
-  lda #"X"             ; Draw cursor
+  lda #"X"             ; draw cursor
   sta display_buffer, x
   jmp next_col
 
 not_cursor:
-  ; Calculate bit position (7 - x)
+  ; calculate bit position (7 - x)
   txa
   eor #7
   tax
@@ -282,19 +402,19 @@ check_revealed:
   and bitmask
   beq draw_hidden
   
-  ; Show revealed number
+  ; show revealed number
   lda number_array, x
   clc
-  adc #"0"             ; Convert to ASCII
+  adc #"0"             ; convert to ascii
   sta display_buffer, x
   jmp next_col
 
 draw_hidden:
-  lda #"."             ; Show hidden cell
+  lda #"."             ; show hidden cell
   sta display_buffer, x
 
 next_col:
-  ldx temp             ; Restore x coordinate
+  ldx temp             ; restore x coordinate
   inx
   cpx #8
   bne col_loop
@@ -322,25 +442,25 @@ neighbor_loop:
   cpx #8
   beq done_counting
 
-; Calculate and validate neighbor X
+; calculate and validate neighbor x
   lda cursor_x
   clc                    
   adc neighbor_x_offset, x
-  bmi skip_neighbor      ; Branch if negative
+  bmi skip_neighbor      ; branch if negative
   cmp #8
-  bcs skip_neighbor      ; Branch if >= 8
+  bcs skip_neighbor      ; branch if >= 8
   sta neighbor_x
 
-; Calculate and validate neighbor Y
+; calculate and validate neighbor y
   lda cursor_y
   clc
   adc neighbor_y_offset, x
-  bmi skip_neighbor      ; Branch if negative
+  bmi skip_neighbor      ; branch if negative
   cmp #8
-  bcs skip_neighbor      ; Branch if >= 8
+  bcs skip_neighbor      ; branch if >= 8
   sta neighbor_y
 
-  ; Create bitmask for neighbor
+  ; create bitmask for neighbor
   lda neighbor_x
   eor #7
   tax
@@ -373,7 +493,7 @@ done_counting:
 ; ====================================
 lcd_wait:
   pha
-  lda #%00000000         ; Set PORTB to input
+  lda #%00000000         ; set PORTB to input
   sta DDRB
 lcdbusy:
   lda #RW
@@ -381,12 +501,12 @@ lcdbusy:
   lda #(RW | E)
   sta PORTA
   lda PORTB
-  and #%10000000         ; Check busy flag
+  and #%10000000         ; check busy flag
   bne lcdbusy
 
   lda #RW
   sta PORTA
-  lda #%11111111         ; Restore PORTB to output
+  lda #%11111111         ; restore PORTB to output
   sta DDRB
   pla
   rts
@@ -394,9 +514,9 @@ lcdbusy:
 execute_lcd_instruction:
   jsr lcd_wait
   sta PORTB
-  lda #0                 ; Clear RS/RW/E
+  lda #0                 ; clear RS/RW/E
   sta PORTA
-  lda #E                 ; Pulse E
+  lda #E                 ; pulse E
   sta PORTA
   lda #0
   sta PORTA
@@ -405,9 +525,9 @@ execute_lcd_instruction:
 print_char:
   jsr lcd_wait
   sta PORTB
-  lda #RS                ; Set RS for character
+  lda #RS                ; set RS for character
   sta PORTA
-  lda #(RS | E)         ; Pulse E
+  lda #(RS | E)          ; pulse E
   sta PORTA
   lda #RS
   sta PORTA
@@ -430,9 +550,18 @@ irq:
 
 select:
   lda #SELECT_INT 
-  sta IFR                ; Clear interrupt flag
+  sta IFR                ; clear interrupt flag
   
-  ; Create bitmask for cursor position
+  lda game_started       ; check if game has started
+  bne normal_select      ; if started, handle normally
+  
+  ; handle startup select
+  lda #1
+  sta game_started       ; set game started flag
+  jmp exit_irq
+  
+normal_select:
+  ; create bitmask for cursor position
   lda cursor_x
   eor #7
   tax
@@ -445,21 +574,20 @@ shift_bitmask3:
   asl bitmask
   dex
   jmp shift_bitmask3
-
 check_mine_hit:
   lda bitmask
-  pha                    ; Save bitmask
+  pha                    ; save bitmask
 
-  ; Check if already revealed
+  ; check if already revealed
   ldx cursor_y
   lda revealed_array, x
   and bitmask
   beq not_revealed
-  pla                    ; Clean up stack
+  pla                    ; clean up stack
   jmp exit_irq
 
 not_revealed:
-  ; Check for mine
+  ; check for mine
   lda mine_array, x
   and bitmask
   beq not_mine
@@ -467,7 +595,7 @@ not_revealed:
   jmp lost
 
 not_mine:
-  ; Calculate buffer position
+  ; calculate buffer position
   lda cursor_y
   asl
   asl
@@ -478,7 +606,7 @@ not_mine:
 
   jsr count_neighbors
 
-  ; Store number
+  ; store number
   ldx buff_ptr
   lda loc_mine_count
   sta number_array, x
@@ -495,7 +623,7 @@ move:
   lda #MOVE_INT
   sta IFR
   
-  lda PORTA             ; Get button state
+  lda PORTA             ; get button state
   and #%00001111
   cmp #LEFT
   beq move_left
